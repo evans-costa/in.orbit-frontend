58d93c21b151bcb2b6c449b7532af9b93febe967
Evandro Costa <evandro.souzac@gmail.com>
2024-09-12 20:12:38 -0300

    feat: add `React-Query` to the project

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 );
