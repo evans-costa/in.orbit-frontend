459d31ae09f7973e7424dcc3d2159edd9b993f03
Evandro Costa <evandro.souzac@gmail.com>
2024-09-12 20:15:43 -0300

    feat: use data provided by fetch on application backend

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,23 @@
-import { Plus } from 'lucide-react';
+import { useQuery } from '@tanstack/react-query';
 
-import logo from './assets/logo.png';
-import letsStart from './assets/let-start-illustration.svg';
+import { CreateGoal } from './components/create-goal';
+import { Summary } from './components/summary';
+import { EmptyGoals } from './components/empty-goals';
+
+import { Dialog } from './components/ui/dialog';
+import { getSummary } from './http/get-summary';
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, // 60 seconds
+  });
+
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="Lets start illustration" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button>
-    </div>
+    <Dialog>
+      {data && data?.total > 0 ? <Summary /> : <EmptyGoals />}
+      <CreateGoal />
+    </Dialog>
   );
 }
